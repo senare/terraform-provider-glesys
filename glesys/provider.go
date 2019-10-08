d5b972ec724319cfc583725e8decae2af6c16eae
Andreas Karlsson <norrland@nullbyte.se>
2019-10-08 10:56:21 +0200

    domainrecord wip

--- glesys/provider.go
+++ glesys/provider.go
@@ -24,6 +24,7 @@ func Provider() terraform.ResourceProvider {
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"glesys_domain":                resourceGlesysDomain(),
+			"glesys_domain_record":         resourceGlesysDomainRecord(),
 			"glesys_loadbalancer":          resourceGlesysLoadBalancer(),
 			"glesys_loadbalancer_backend":  resourceGlesysLoadBalancerBackend(),
 			"glesys_loadbalancer_frontend": resourceGlesysLoadBalancerFrontend(),
